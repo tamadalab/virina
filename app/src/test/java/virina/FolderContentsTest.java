8670ee1f56c95f71f8b6f6aeaf162c72638a47c1
山崎駿 <yamazakishun@yamazakishunnoMacBook-Air.local>
2023-07-25 11:10:11 +0900

    テストコードの途中経過です。

--- app/src/test/java/virina/FolderContentsTest.java
+++ app/src/test/java/virina/FolderContentsTest.java
@@ -2,6 +2,9 @@ package virina;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.nio.file.Path;
+
 // import java.nio.file.Path;
 
 // import static org.junit.jupiter.api.Assertions.*;
@@ -11,10 +14,12 @@ class FolderContentsTest {
     @Test
     void main() {
         FolderContents testMain = new FolderContents();
-        testMain.main("./");
+        testMain.main("~/Desktop/test");
     }
 
     @Test
     void api() {
+        FolderContents testApi = new FolderContents();
+        testApi.api(Path.of("./AppTest.java").toFile());
     }
 }
