db42607002ceef259e3fd6e9f27f418d6116fdf0
山崎駿 <yamazakishun@yamazakishunnoMacBook-Air.local>
2023-07-25 11:07:19 +0900

    コード作成の途中経過です。

--- app/src/main/java/virina/FolderContents.java
+++ app/src/main/java/virina/FolderContents.java
@@ -1,17 +1,39 @@
 package virina;
 
+import java.io.BufferedReader;
 import java.io.File;
+//import org.apache.http.client.methods.HttpPost;
+//import org.apache.http.entity.StringEntity;
+//import org.apache.http.impl.client.HttpClients;
+import java.io.FileReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+//import java.nio.file.Path;
 
-public class FolderContents{
-    private static final String API_URL = "https://api.openai.com/v1/engines/gpt-4.0/completions";
-    private static final String API_KEY = "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";
-    public static void main(String... args) {
+
+public class FolderContents
+{
+    class Api{
+        // 使用するapiのエンジンの記載
+        private final String API_URL = "https://api.openai.com/v1/engines/gpt-3.5/completions";
+        // api keyの記載
+        private static final String API_KEY = "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";
+
+    }
+    public void main(String... args)
+    {
         if (args.length == 0) {
             System.out.println("フォルダを指定してください。");
-            return;
+//            return;
+        } else {
+            this.fileChecker(args[0]);
         }
-
-        String folderPath = args[0];
+    }
+    public void fileChecker(String folders)
+    {
+        String folderPath = folders;
         File folder = new File(folderPath);
 
         if (!folder.exists()) {
@@ -29,16 +51,53 @@ public class FolderContents{
         if (files != null && files.length > 0) {
             System.out.println("フォルダの中身:");
             for (File file : files) {
-                api(file);
-                // System.out.println(file.getName());
+                if(file.exists()) {
+                    this.api(file);
+                    // System.out.println(file.getName());
+                }
             }
-        } else {
+        }
+        else {
             System.out.println("フォルダは空です。");
         }
     }
-    public static void api(File file)
-    {
-        System.out.println(file.getName());
-        
+
+    public void api(File file) {
+        try {
+            Api api = new Api();
+            String apiUrl = api.API_URL;
+
+            // ファイルからソースコードを読み取る
+            String sourceCodeFilePath = file.toString();
+            StringBuilder sourceCodeContent = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new FileReader(sourceCodeFilePath))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sourceCodeContent.append(line).append("\n");
+                }
+            }
+
+            // 質問内容
+            String question = "You will be asked to determine if the source code you are about to present was written by you.If it is, answer \"yes\"; if not, answer \"no\".";
+
+            // ソースコードと質問をパッケージ化
+            String payload = "{\"question\": \"" + question + "\", \\\"source_code\\\": \\\"\" + sourceCodeContent.toString() + \"\\\"}";
+
+            // httpクライアントの初期化
+            HttpClient httpClient = HttpClient.newHttpClient();
+
+            // POSTリクエストを作成し、パッケージ化したデータを送信
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(apiUrl))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(payload))
+                    .build();
+
+            // APIにリクエストを送信
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response);
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
     }
 }
