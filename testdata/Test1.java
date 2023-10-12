c6ab9d5287524aa2a2f36a8b779ce35ad50f22c7
山崎駿 <yamazakishun@yamazakishunnoMacBook-Air.local>
2023-10-12 12:07:23 +0900

    テスト用のソースコードを変更しました.

--- testdata/Test1.java
+++ testdata/Test1.java
@@ -1,5 +1,22 @@
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // 引数が提供されているかどうかを確認
+        if (args.length < 1) {
+            System.out.println("エラー: 文字列引数を入力してください");
+            System.exit(1);  // エラーコード 1 でプログラムを終了
+        }
+
+        // 引数で渡された最初の文字列を取得
+        String inputString = args[0];
+
+        // 入力文字列を出力
+        System.out.println("入力文字列: " + inputString);
+
+        // 文字列を逆順にする
+        String reversedString = new StringBuilder(inputString).reverse().toString();
+
+        // 逆順の文字列を出力
+        System.out.println("逆順文字列: " + reversedString);
     }
 }
+
